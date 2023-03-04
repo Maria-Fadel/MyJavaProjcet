8a7baecea35aa52d766f41d12fcf2a90e2b98305
Maria-Fadel <ichbinkieler@gmail.com>
2023-03-04 10:03:20 +0100

    the second commit

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Guten Tag from Github");
+		System.out.println("Guten Tag from Github");
 	}
 
 }
